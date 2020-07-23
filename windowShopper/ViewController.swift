b1801bffefd248b0b79ffa046cdc29af8924af7f
Sophie Popow <sophie.popow@gmail.com>
2020-07-23 14:56:09 +0200

    initial xcode project

---
+++ windowShopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  windowShopper
+//
+//  Created by Sophie Popow on 23/07/2020.
+//  Copyright © 2020 Sophie Popow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
