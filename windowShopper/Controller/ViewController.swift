fea30095d0b0a30d3a0f499203ff8897d10621ee
Sophie Popow <sophie.popow@gmail.com>
2020-07-26 19:51:18 +0200

    adding Calculate button

--- windowShopper/Controller/ViewController.swift
+++ windowShopper/Controller/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var wageTextField: CurrencyTextField!
+    
+    @IBOutlet weak var priceTextField: CurrencyTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let calcButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcButton.backgroundColor = #colorLiteral(red: 1, green: 0.5782525589, blue: 0, alpha: 1)
+        calcButton.setTitle("Calculate", for: .normal)
+        calcButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcButton.addTarget(self, action: #selector(ViewController.calculate), for: .touchUpInside)
+        
+        wageTextField.inputAccessoryView = calcButton
+        priceTextField.inputAccessoryView = calcButton
     }
 
+    @objc func calculate() {
+        
+    }
 
 }
 
--- windowShopper/Views/CurrencyTextField.swift
+++ windowShopper/Views/CurrencyTextField.swift
@@ -8,10 +8,18 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTextField: UITextField {
 
+    override func prepareForInterfaceBuilder() {
+        customizedView()
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizedView()
+    }
+    
+    func customizedView() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
@@ -21,7 +29,5 @@ class CurrencyTextField: UITextField {
             attributedPlaceholder = placehold
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
-        
-
     }
 }
